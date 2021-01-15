92847031978268628d51092216a33310ef9a7832
Julian Schäfer <schaefer@synyx.de>
2021-01-15 11:09:25 +0100

    Add tailwind as dependency

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [
+    './template/**/*.html',
+    './partials/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
