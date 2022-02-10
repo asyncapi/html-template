8b1302d9921dda80da8884fe7d7516c2ffada986
ritik307 <ritikpr307@gmail.com>
2022-02-10 21:31:39 +0530

    made requested changes

--- components/common.js
+++ components/common.js
@@ -14,15 +14,18 @@ ${childrenContent}
 }
 
 export function Head({ title, cssLinks = [], styleContent = [],base = "" }) {
+  const baseTag= `<base href=${base}>`;
+  const cssLinksData = `\n<link href=${cssLinks[0]}>\n<link href=${cssLinks[1]}>\n`;
+  const styleContentData = `<style>\n${styleContent[0]}\n${styleContent[1]}</style>`;
   const content = `
 <head>
   <meta charset="utf-8">
-  ${base && `<base href=${base}>`}
+  ${base && baseTag}
   <title>${title}</title>
 ${withIndendation(links, 2, IndentationTypes.SPACES)}
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  ${(styleContent.length > 0) ? `<style>\n${styleContent[0]}\n${styleContent[1]}</style>` : ""}
-  ${(cssLinks.length > 0) ? `\n<link href=${cssLinks[0]}>\n<link href=${cssLinks[1]}>\n` : ""}
+  ${(styleContent.length > 0) ? styleContentData : ""}
+  ${(cssLinks.length > 0) ? cssLinksData : ""}
 </head>  
 `;
 
