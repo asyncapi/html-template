b822ffdcdf522621c6bc402b40088d89fd58e000
Mintu <mintugogoi567@gmail.com>
2024-03-23 17:55:53 +0530

    added the transpile script

--- scripts/copy-sources.js
+++ scripts/copy-sources.js
@@ -5,19 +5,33 @@
  */
 
 const util = require('util');
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 const copyFile = util.promisify(fs.copyFile);
+const { exec } = require('child_process');
 
 // source (node_modules): destination (template)
 const filesToCopy = {
-  "@asyncapi/react-component/browser/standalone/without-parser.js": "js/asyncapi-ui.min.js",
-  "@asyncapi/react-component/styles/default.min.css": "css/asyncapi.min.css",
+  "node_modules/@asyncapi/react-component/browser/standalone/without-parser.js": "js/asyncapi-ui.min.js",
+  "node_modules/@asyncapi/react-component/styles/default.min.css": "css/asyncapi.min.css",
 };
 
 async function copyFiles() {
+  await new Promise((resolve) => {
+    exec('npm run transpile', (err, stdout, stderr) => {
+      if (err) {
+        console.error(`Error during template transpilation: ${err}`);
+        return;
+      }
+      if (stdout) console.log(stdout);
+      if (stderr) console.log('Template transpilation error:', stderr);
+      resolve();
+    });
+  });
   const operations = Object.entries(filesToCopy).map(([source, destination]) => {
-    return copyFile(path.join(__dirname, '../node_modules', source), path.join(__dirname, '../template', destination));
+    const sourcePath = path.join(__dirname, '../', source);
+    const destinationPath = path.join(__dirname, '../template', destination);
+    return fs.copy(sourcePath, destinationPath, { overwrite: true });
   });
   await Promise.all(operations);
 }
---
+++ scripts/transpile.js
@@ -0,0 +1,15 @@
+const { transpileFiles } = require('@asyncapi/generator-react-sdk');
+const path = require('path')
+
+async function transpileTemplate() {
+  const templateContentDir = path.join(__dirname, '../template');
+  console.log("templateContentDir",templateContentDir)
+  const outputDir = path.join(__dirname, '../__transpiled');
+  console.log("outputDir",outputDir)
+
+  await transpileFiles(templateContentDir, outputDir, { recursive: true });
+}
+
+transpileTemplate().catch((err) => console.log('Error during template transpilation:', err));
+
+module.exports = { transpileTemplate }
