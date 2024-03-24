eff727c7dda7f74ba1ffb8a0e11a324624b3b6f1
Mintu <mintugogoi567@gmail.com>
2024-03-24 14:58:19 +0530

    added files key in package.json and remove changes made in copy-files

--- scripts/copy-sources.js
+++ scripts/copy-sources.js
@@ -8,7 +8,6 @@ const util = require('util');
 const fs = require('fs');
 const path = require('path');
 const copyFile = util.promisify(fs.copyFile);
-const { main } = require('./transpile')
 
 // source (node_modules): destination (template)
 const filesToCopy = {
@@ -17,7 +16,6 @@ const filesToCopy = {
 };
 
 async function copyFiles() {
-  await main()
   const operations = Object.entries(filesToCopy).map(([source, destination]) => {
     return copyFile(path.join(__dirname, '../node_modules', source), path.join(__dirname, '../template', destination));
   });
--- scripts/transpile.js
+++ scripts/transpile.js
@@ -1,19 +1,11 @@
 const { transpileFiles } = require('@asyncapi/generator-react-sdk');
 const path = require('path')
 
-async function main(){
-  async function transpileTemplate() {
-    const templateContentDir = path.join(__dirname, '../template');
-    console.log("templateContentDir",templateContentDir)
-    const outputDir = path.join(__dirname, '../__transpiled');
-    console.log("outputDir",outputDir)
-  
-    await transpileFiles(templateContentDir, outputDir, { recursive: true });
-  }
-  
-  transpileTemplate().catch((err) => console.log('Error during template transpilation:', err));
+async function transpileTemplate() {
+  const templateContentDir = path.join(__dirname, '../template');
+  console.log("templateContentDir",templateContentDir)
+  const outputDir = path.join(__dirname, '../__transpiled');
+  await transpileFiles(templateContentDir, outputDir, {recursive: true})
 }
 
-main()
-
-module.exports = { main }
+transpileTemplate().catch((err) => console.log('Error during template transpilation:', err));
