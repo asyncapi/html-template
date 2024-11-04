8a93359d8f2037abcb1b22833a07c37d0b17926d
Michael Wiesenbauer <11874984+mwiesenbauer@users.noreply.github.com>
2024-11-04 19:22:17 +0100

    feat: render react app setup into dedicated js file (#686)

--- test/components/index.test.js
+++ test/components/index.test.js
@@ -1,6 +1,6 @@
 import { render } from '@asyncapi/generator-react-sdk';
 import { Parser, fromFile } from '@asyncapi/parser';
-import { Index } from '../../components/index';
+import {App, Index} from '../../components/index';
 import path from 'path';
 const asyncapi_v3_path = path.resolve(__dirname, '../spec/asyncapi_v3.yml');
 const asyncapi_v2_path = path.resolve(__dirname, '../spec/asyncapi_v2.yml');
@@ -19,6 +19,24 @@ describe('Index component', () => {
     const { document } = await fromFile(parser, asyncapi_v2_path).parse();
     expect(document).not.toBeUndefined();
     const result = render(<Index asyncapi={document} params={{}} />);
+    const actual = result.trim();
+    expect(actual).toMatchSnapshot();
+  });
+});
+
+describe('App component', () => {
+  it('should render AsyncAPI v3 document', async () => {
+    const { document } = await fromFile(parser, asyncapi_v3_path).parse();
+    expect(document).not.toBeUndefined();
+    const result = render(<App asyncapi={document} params={{}}/>);
+    const actual = result.trim();
+    expect(actual).toMatchSnapshot();
+  });
+
+  it('should render AsyncAPI v2 document', async () => {
+    const { document } = await fromFile(parser, asyncapi_v2_path).parse();
+    expect(document).not.toBeUndefined();
+    const result = render(<App asyncapi={document} params={{}} />);
     const actual = result.trim();
     expect(actual).toMatchSnapshot();
   });
